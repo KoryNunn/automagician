75dfe8e105c28eaf7142aaf807ac00ecc73b3dca
Kory Nunn <knunn187@gmail.com>
2018-11-29 10:28:46 +1000

    Latest

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+var config = require('config');
 var http = require('http');
 var fs = require('fs');
-var port = process.argv[2] || 8080;
 
 var server = http.createServer(function(request, response){
 	fs.createReadStream(__dirname + '/index.browser.js').pipe(response);
 });
 
-server.listen(port);
+server.listen(config.get('port'));
